68d7ab1b0ad63873756a242b9b4ba4bb9c5feee1
kaizawa97 <kai109870@gmail.com>
2021-08-17 14:48:30 +0900

    update dotenv

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 'use strict';
 const express = require('express');
 const helmet = require('helmet');
+require('dotenv').config();
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 app.use(helmet());
 
 app.get('/', (req, res) => {
