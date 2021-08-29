d937540a29653ae2a139af855f3ceffad5ad060e
kaizawa97 <kai109870@gmail.com>
2021-08-29 22:38:15 +0900

    update client

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react';
+// import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+const App = () => {
+  
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
