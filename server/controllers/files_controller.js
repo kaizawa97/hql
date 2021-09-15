ec52971a56453cd936fe73d7e1b8a4c903273d49
kaizawa97 <kai109870@gmail.com>
2021-09-15 11:15:35 +0900

    update uploader

--- server/controllers/files_controller.js
+++ server/controllers/files_controller.js
@@ -1,4 +1,5 @@
 const multer = require('multer');
+const path = require('path');
 require('dotenv').config();
 
 const storage = multer.diskStorage({
@@ -7,9 +8,18 @@ const storage = multer.diskStorage({
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Math.round(Math.random() * 1E9);
-    cb(null, String(Date.now() + uniqueSuffix));
+    cb(null, String(Date.now() + uniqueSuffix + path.extname(file.originalname)));
   }
 });
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    req.fileValidationError = 'goes wrong on the mimetype';
+    cb(null, false);
+  }
+};
 
 exports.upload = multer({
   storage: storage,
@@ -17,16 +27,24 @@ exports.upload = multer({
     fileSize: 5000000,
     files: 10,
     fields: 10
-  }
+  },
+  fileFilter: fileFilter
 }).single('image');
 
 exports.createImage = (req, res) => {
-  if (!req.file) {
-    console.log("No file upload");
-    return res.status(400).send('No file upload');
-  } else {
-    console.log(req.file.filename)
+  console.log(req);
+  if (req.fileValidationError) {
+    return res.status(400).json({
+      message: "only jpg or png"
+    });
   }
+  if (req.file) {
+    return res.status(200).send(req.file.filename);
+  } else {
+    return res.status(400).json({
+      message: "noFileUploaded"
+    });
+  }  
 };
 
 exports.getImageById = (req, res) => {
