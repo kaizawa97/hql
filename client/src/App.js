28191ea01582ddf51ce1d357e2840cca27064560
kaizawa97 <kai109870@gmail.com>
2022-01-12 16:07:42 +0900

    update client

--- client/src/App.js
+++ client/src/App.js
@@ -1,62 +1,58 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import axios from 'axios';
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.css';
 
-import { Home } from './pages/home';
-import { Signin } from './pages/signin';
-import { Signup } from './pages/signup';
-import { Posts } from './pages/posts';
-import { Notfound } from './pages/error'
-// import { Profile } from './pages/profile';
-
-export const App = () => {
-  const [loggedInStatus, setLoggedInStatus] = useState("Not Logged In");
-  const [user, setUser] = useState({});
-  const handleLogin = (data) => {
-    setLoggedInStatus("Logged In");
-    setUser(data.user);
-  };
-
-  useEffect(() => {
-    checkLoginStatus();
-  });
-
-  const checkLoginStatus = () => {
-    axios.get('http://localhost:5000/api/v1/isLogined', {withCredentials: true})
-      .then(res => {
-        if (res.data.logged_in && loggedInStatus === "Not Logged In") {
-          setLoggedInStatus("Logged In");
-          setUser(res.data.user);
-        } else if (!res.data.logged_in && loggedInStatus === "Logged In") {
-          setLoggedInStatus("Not Logged In");
-          setUser({});
-        }
-        console.log("logged in res", res);
-      }).catch(err => {
-        console.log("logged in err", err);
-      })
-  }
-
-  const handleLogout = () => {
-    setLoggedInStatus("Not Logged In");
-    setUser({});
-    axios.delete('http://localhost:5000/api/v1/logout', { withCredentials: true })
-      .then(res => {
-        console.log("logout res", res);
-      }).catch(err => {
-        console.log("logout err", err);
-      })
-  }
-
+function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home handleLogin={handleLogin} loggedInStatus={loggedInStatus} />} />
-        <Route path='/signin' element={<Signin />} />
-        <Route path='/signup' element={<Signup handleLogin={handleLogin}/>} />
-        <Route path='/posts' element={<Posts />} />
-        <Route path='*' celement={<Notfound />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Counter />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <span>
+          <span>Learn </span>
+          <a
+            className="App-link"
+            href="https://reactjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux-toolkit.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux Toolkit
+          </a>
+          ,<span> and </span>
+          <a
+            className="App-link"
+            href="https://react-redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React Redux
+          </a>
+        </span>
+      </header>
+    </div>
   );
 }
+
+export default App;
--- client/src/pages/error.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const Notfound = () => {
-  return (
-    <h1>Not Found!!</h1>
-  );
-}
