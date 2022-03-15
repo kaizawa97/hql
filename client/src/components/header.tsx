7fe2f64173e1d2857d2a76e4f75110eb156430e6
kaizawa97 <kai109870@gmail.com>
2022-03-15 19:10:48 +0900

    style: add ChakraUI and update header

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,5 +1,76 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import { Flex, Box, Button, ButtonGroup, Spacer } from "@chakra-ui/react";
 
-export const Header = () => {
+type Props = {
+  authState?: boolean
+}
+
+export const Header = ({ authState = false }: Props) => {
+  if (authState == true) {
+    return (
+      <div>
+        <Flex bg="White" w="100%">
+          <Flex
+            as="header"
+            position="fixed"
+            width="full"
+            shadow="sm"
+            py={4}
+            px={8}
+          >
+            <Box>
+              <Button as={Link} to="/">
+                <img src="../logo.png" alt="HQL" />
+              </Button>
+            </Box>
+            <Spacer />
+            <Box>
+              <ButtonGroup spacing={4}>
+                <Button colorScheme="blue">
+                  Signin
+                </Button>
+                <Button colorScheme="blue">
+                  Signup
+                </Button>
+              </ButtonGroup>
+            </Box>
+          </Flex>
+        </Flex>
+      </div>
+    )
+  }
+  else {
+    return (
+      <div>
+        <Flex bg="White" w="100%">
+          <Flex
+            as="header"
+            position="fixed"
+            width="full"
+            shadow="sm"
+            py={4}
+            px={8}
+          >
+            <Box>
+              <Button as={Link} to="/">
+                <img src="../logo.png" alt="HQL" />
+              </Button>
+            </Box>
+            <Spacer />
+            <Box>
+              <ButtonGroup spacing={4}>
+                <Button colorScheme="blue">
+                  Signin
+                </Button>
+                <Button colorScheme="blue">
+                  Signup
+                </Button>
+              </ButtonGroup>
+            </Box>
+          </Flex>
+        </Flex>
+      </div>
+    )
+  }
 };
--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,14 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Flex, Box, Button, ButtonGroup, Spacer, Heading } from "@chakra-ui/react";
 
 import { Header } from '../components/header';
 import { Footer } from '../components/footer';
 // import { useAuth } from '../hooks/useAuth';
 
 export const Home = () => {
+  const auth = true;
   return (
     <div>
-      <h1>Join NCN Group today.</h1>
+      <Header authState={auth}/>
+      <Box mt={"6rem"} mx="auto">
+        <Heading as="h1" size="lg" fontWeight="bold">
+          Welcome
+        </Heading>
+      </Box>
     </div>
   );
 }
